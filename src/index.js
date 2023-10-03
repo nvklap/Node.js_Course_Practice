b8a718c8273364015638049ef3e3611a44ef0e79
Nataliia Klapchuk <nataklap@gmail.com>
2023-10-03 15:02:52 +0300

    Add health-check route

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const healthRouter = require('./routes/health-check');
+
 app.get('/', (req, res) => {
 	res.send('<a href="/health-check">Health<a>');
 });
+
+app.use('/health-check', healthRouter);
 
 app.listen(PORT, () => {
 	`Server is listening on ${PORT} port`;
