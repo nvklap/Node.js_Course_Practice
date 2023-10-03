6af6e9d9254780c58a83b245b1ee8a395efae066
Nataliia Klapchuk <nataklap@gmail.com>
2023-10-03 15:37:36 +0300

    Document /health-check endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
-const app = express();
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Epam Node js Course',
+			version: '1.0.0',
+		},
+		servers: [
+			{ url: 'http://localhost:3000', description: 'Development server' },
+		],
+	},
+	apis: ['./src/routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 const healthRouter = require('./routes/health-check');
@@ -8,6 +25,8 @@ const healthRouter = require('./routes/health-check');
 app.get('/', (req, res) => {
 	res.send('<a href="/health-check">Health<a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use('/health-check', healthRouter);
 
